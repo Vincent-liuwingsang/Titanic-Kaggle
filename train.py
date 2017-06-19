2e33727deecd42289dcb633cb72c53ca37fc323c
Vincent <wing.liu12@imperial.ac.uk>
2017-06-19 22:37:29 +0800

    Added codes for tunning with GridSearchCV

--- train.py
+++ train.py
@@ -1,51 +1,108 @@
+import re
 import xgboost as xgb
 import pandas as pd
 import numpy as np
 import sklearn.model_selection as ms
-
-
+import sklearn.preprocessing as pp
+from sklearn import metrics
+import matplotlib.pylab as plt
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 total=pd.concat([train,test])
+PassengerId=total['PassengerId']
+
+def fill_features(df):
+    df['Embarked'].fillna('S',inplace=True)
+    df['Fare'].fillna(df['Fare'].median(),inplace=True)
+    mu=df['Age'].mean()
+    delta=df['Age'].std()
+    rand_list=np.random.randint(mu-delta,mu+delta,size=df['Age'].isnull().sum())
+    df.loc[df['Age'].isnull(), 'Age']=rand_list
+    df['Age']=df['Age'].astype(int)
+    return df
+
+def add_features(df):
+    df['FamilySize']=df['SibSp']+df['Parch']+1
+    df['IsAlone']=1
+    df.loc[df['FamilySize']>1, 'IsAlone']=0
+    df['HasCabin']=df['Cabin'].apply(lambda x: 0 if type(x)==float else 1)
+    return df
+
+def simplify_features(df):
+    df=simplify_fare(df)
+    df=simplify_ages(df)
+    df=simplify_title(df)
+    return df
+
+def simplify_fare(df):
+    bins=(-1,0,7.896,14.454,31.275,512.4)
+    tags=['unknown', 'first','second','third','forth']
+    df['Fare']=pd.cut(df['Fare'], bins, tags)
+    return df	
+
+def simplify_ages(df):
+    bins=(-1,0,5,12,18,25,35,60,120)
+    tags = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+    df['Age'] = pd.cut(df['Age'],bins , labels=tags) 
+    return df
 
 
-total['FamilySize']=total['SibSp']+total['Parch']+1
-total['IsAlone']=1
-total.loc[total['FamilySize']>1, 'IsAlone']=0
-total['Embarked'].fillna('S',inplace=True)
-total['Fare'].fillna(total['Fare'].median(),inplace=True)
-mu=total['Age'].mean()
-delta=total['Age'].std()
-rand_list=np.random.randint(mu-delta,mu+delta,size=total['Age'].isnull().sum())
-total.loc[total['Age'].isnull(), 'Age']=rand_list
-total['Age']=total['Age'].astype(int)
-total.drop('PassengerId',axis=1,inplace=True)
-total.drop('Name',axis=1,inplace=True)
-total.drop('Cabin',axis=1,inplace=True)
-total.drop('SibSp',axis=1,inplace=True)
-total.drop('Parch',axis=1,inplace=True)
-total.drop('Ticket',axis=1,inplace=True)
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
 
+def simplify_title(df):
+    df['Title'] = df['Name'].apply(get_title)
+    df['Title'] = df['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    df['Title'] = df['Title'].replace('Mlle', 'Miss')
+    df['Title'] = df['Title'].replace('Ms', 'Miss')
+    df['Title'] = df['Title'].replace('Mme', 'Mrs')
+    return df
 
-age_bins=[-1,16,32,48,64,80]
-total['Age'] = pd.cut(total['Age'],age_bins , labels=range(len(age_bins)-1)) 
+def drop_features(df):
+    df.drop('PassengerId',axis=1,inplace=True)
+    df.drop('Name',axis=1,inplace=True)
+    df.drop('Cabin',axis=1,inplace=True)
+    df.drop('SibSp',axis=1,inplace=True)
+    #total.drop('Parch',axis=1,inplace=True)
+    df.drop('Ticket',axis=1,inplace=True)
+    return df
 
-fare_bins=[-1,7.896,14.454,31.275,512.3292]
-total['Fare']=pd.cut(total['Fare'], fare_bins, labels=range(len(fare_bins)-1))
+def encode_features(df):
+    features = ['Fare', 'Age', 'Embarked', 'Sex', 'Title']
+    for feature in features:
+        le=pp.LabelEncoder()
+        le=le.fit(df[feature])
+        df[feature]=le.transform(df[feature])
+    return df
 
-total['Embarked']=total['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2 }).astype(int)
-total['Sex']=total['Sex'].map( { 'female' : 0 , 'male' : 1 } ).astype(int)
-
+total=fill_features(total)
+total=add_features(total)
+total=simplify_features(total)
+total=drop_features(total)
+total=encode_features(total)
 
 train_y = train['Survived']
-
 total.drop('Survived', axis=1, inplace=True)
-total_array = total.as_matrix()
+train_x=total.iloc[range(len(train_y))]
 
-train_x = total_array[:len(train_y),:]
-test_x = total_array[len(train_y):,:]
+param_test1 = {
+	'gamma':[0.175,0.2,0.225]
+}
+gsearch1 = ms.GridSearchCV(estimator = xgb.XGBClassifier( learning_rate =0.1, n_estimators=46, max_depth=3,
+ min_child_weight=0, gamma=0.2, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', n_jobs=4, scale_pos_weight=1,random_state=29), 
+ param_grid = param_test1, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch1.fit(train_x, train_y)
+print gsearch1.cv_results_
+print gsearch1.best_params_
+print gsearch1.best_score_
 
+"""
 #param_grid = {'max_depth':range(3,10,1), 'min_child_weight':range(1,6,1)} # 3,4
 #param_grid = {'gamma':[i/10.0 for i in range(0,7)]} # 0.6
 #param_grid = {'subsample':[i/100.0 for i in range(60,100,5)], 'colsample_bytree':[i/100.0 for i in range(60,100,5)]} # 0.85,0.75
@@ -63,11 +120,11 @@ model = xgb.XGBClassifier(learning_rate=0.1,
                       n_jobs=4, 
                       scale_pos_weight=1)
 
-#grid = ms.GridSearchCV(estimator=model, param_grid=param_grid, scoring='roc_auc', n_jobs=-1, verbose=1)
-#grid.fit(train_x,train_y)
+grid = ms.GridSearchCV(estimator=model, param_grid=param_grid, scoring='roc_auc', n_jobs=-1, verbose=1)
+grid.fit(train_x,train_y)
 model.fit(train_x,train_y)
 predictions = model.predict(test_x)
 submission = pd.DataFrame({ 'PassengerId': test['PassengerId'],
                             'Survived': predictions })
 submission.to_csv("submission.csv", index=False)
-
+"""
